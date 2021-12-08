49cd7de8471eeb21e49d6f0ebd74567878fa53ed
jeon0728 <jeon0728@gmail.com>
2021-12-09 00:23:35 +0900

    test

---
+++ styling-react/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SassComponents from "./SassComponents";
+
+const App = () => {
+  return (
+    <div>
+      <SassComponents></SassComponents>
+    </div>
+  );
+};
+
+export default App;
